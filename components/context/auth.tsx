a2695e60d1057caf7b35bd75aa0e61c7cd60dece
PRONIN Vladimir <vladimir.pronin@raiffeisen.ru>
2022-11-06 18:25:03 +0300

    fix: fix auth

--- components/context/auth.tsx
+++ components/context/auth.tsx
@@ -1,36 +1,12 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 
-import { getAuth, onAuthStateChanged } from "firebase/auth";
-import { app } from "pages/_app";
-import Router from "next/router";
 import Spinner from "components/spinner";
+import { useAuth } from "./hooks/use-auth";
 
 export const AuthContext = React.createContext({});
 
 export const AuthProvider: React.FC<any> = ({ children }) => {
-    const [currentUser, setCurrentUser] = useState(null);
-    const [pending, setPending] = useState(true);
-
-    useEffect(() => {
-        (async () => {
-            const auth = getAuth(app);
-            //@ts-ignore
-            await onAuthStateChanged(auth, async (user) => {
-                if (user) {
-                    //@ts-ignore
-                    setCurrentUser(user);
-                } else {
-                    const pathname = Router.pathname.toLowerCase();
-                    if (pathname === "/login" || pathname === "/signup") {
-                    } else {
-                        Router.replace("/login");
-                    }
-                }
-            });
-
-            setPending(false);
-        })();
-    }, []);
+    const { currentUser, setCurrentUser, pending } = useAuth();
 
     if (pending) {
         return <Spinner />;
@@ -47,26 +23,3 @@ export const AuthProvider: React.FC<any> = ({ children }) => {
         </AuthContext.Provider>
     );
 };
-
-// function Loading() {
-//     const router = useRouter();
-
-//     const [loading, setLoading] = useState(false);
-
-//     useEffect(() => {
-//         const handleStart = (url) => (url !== router.asPath) && setLoading(true);
-//         const handleComplete = (url) => (url === router.asPath) && setLoading(false);
-
-//         router.events.on('routeChangeStart', handleStart)
-//         router.events.on('routeChangeComplete', handleComplete)
-//         router.events.on('routeChangeError', handleComplete)
-
-//         return () => {
-//             router.events.off('routeChangeStart', handleStart)
-//             router.events.off('routeChangeComplete', handleComplete)
-//             router.events.off('routeChangeError', handleComplete)
-//         }
-//     })
-
-//     return loading && (<div>Loading....{/*I have an animation here*/}</div>);
-// }
---
+++ components/context/hooks/use-auth.tsx
@@ -0,0 +1,45 @@
+import React, { useEffect, useState } from "react";
+
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+import { app } from "pages/_app";
+import Router from "next/router";
+
+export const AuthContext = React.createContext({});
+
+export const useAuth = () => {
+    const [currentUser, setCurrentUser] = useState(null);
+    const [pending, setPending] = useState(true);
+
+    useEffect(() => {
+        const auth = getAuth(app);
+        //@ts-ignore
+        const unsubscribe = onAuthStateChanged(auth, async (user) => {
+            if (user) {
+                //@ts-ignore
+                setCurrentUser(user);
+            } else {
+                const pathname = Router.pathname.toLowerCase();
+                if (pathname === "/login" || pathname === "/signup") {
+                    setPending(false);
+                } else {
+                    Router.push("/login");
+                }
+            }
+        });
+
+        return unsubscribe;
+    }, []);
+
+    useEffect(() => {
+        const handlePageChangeComplete = () => setPending(false);
+
+        Router.events.on("routeChangeComplete", handlePageChangeComplete);
+
+        return Router.events.off(
+            "routeChangeComplete",
+            handlePageChangeComplete
+        );
+    }, []);
+
+    return { currentUser, setCurrentUser, pending };
+};
